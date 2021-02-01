c7b38bd82cde0f46cef2ad9c866712ce51c97fe3
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-01 20:33:41 +0900

    feat(command): add request

--- src/main/java/me/saidbysolo/study/Command.java
+++ src/main/java/me/saidbysolo/study/Command.java
@@ -5,19 +5,42 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class Command implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-        if (sender instanceof Player){
+        if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (label.equalsIgnoreCase("o")){
-                player.sendMessage("O");
-                Bukkit.broadcastMessage("Test Message!");
+            try {
+                URL url = new URL("https://doujinshiman.ga");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.setDoOutput(false);
+                StringBuilder sb = new StringBuilder();
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(con.getInputStream(), "utf-8"));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                br.close();
+
+                if (label.equalsIgnoreCase("o")) {
+                    player.sendMessage("O");
+                    Bukkit.broadcastMessage("" + sb.toString());
+
+                }
+            } catch (Exception e) {
+                System.err.println(e.toString());
             }
-            else if (label.equalsIgnoreCase("x")){
-                player.sendMessage("X");
-            }
+
         }
+
         return false;
     }
 }
