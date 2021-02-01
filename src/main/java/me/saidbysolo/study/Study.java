f8e3c5562e94e48a74c0ff86b4eea8fc7d013b65
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-01 10:48:05 +0900

    feat(Study): add main class

---
+++ src/main/java/me/saidbysolo/study/Study.java
@@ -0,0 +1,17 @@
+package me.saidbysolo.study;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Study extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
