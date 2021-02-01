be468be7e72f5d96b3eae775858270daeb73eca1
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-01 11:36:22 +0900

    feat(Study): add logger

--- src/main/java/me/saidbysolo/study/Study.java
+++ src/main/java/me/saidbysolo/study/Study.java
@@ -1,12 +1,13 @@
 package me.saidbysolo.study;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Study extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getLogger().info("asdf");
 
     }
 
