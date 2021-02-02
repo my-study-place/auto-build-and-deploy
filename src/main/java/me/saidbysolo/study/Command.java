f6eca5ed8d53bf6bed132945693ed82b5be67c6a
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-02 20:35:30 +0900

    feat(command): test for read json

--- src/main/java/me/saidbysolo/study/Command.java
+++ src/main/java/me/saidbysolo/study/Command.java
@@ -1,18 +1,36 @@
 package me.saidbysolo.study;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.io.FileReader;
 
 public class Command implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         if (sender instanceof Player){
             Player player = (Player) sender;
+            JSONParser parser = new JSONParser();
+
             if (label.equalsIgnoreCase("o")){
                 player.sendMessage("O");
                 Bukkit.broadcastMessage("Test Message!");
+                try {
+                    Object obj = parser.parse(new FileReader("C:/Users/saidb/Desktop/asdf.json"));
+                    JSONObject jsonObject = (JSONObject) obj;
+
+                    String data = (String) jsonObject.get("data");
+
+                    Bukkit.broadcastMessage(data);
+
+                }catch (Exception e){
+
+                }
+
             }
             else if (label.equalsIgnoreCase("x")){
                 player.sendMessage("X");
