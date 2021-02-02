e5a868f4c9c44180dff16836e14b57428165090b
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-02 22:04:32 +0900

    Revert "Merge pull request #1 from MCHDF/patch-1"
    This reverts commit daa97b1333472f1f64fd995e77eac31c87956fb0, reversing
    changes made to cc8f70b28a4a775d92344587317e1b29c03e1f53.

--- src/main/java/me/saidbysolo/study/Command.java
+++ src/main/java/me/saidbysolo/study/Command.java
@@ -3,7 +3,6 @@ package me.saidbysolo.study;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -12,7 +11,7 @@ import java.io.FileReader;
 
 public class Command implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         if (sender instanceof Player){
             Player player = (Player) sender;
             JSONParser parser = new JSONParser();
