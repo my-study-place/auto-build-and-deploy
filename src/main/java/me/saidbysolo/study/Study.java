1e10f19aadcbc46433e8c009018192dc7e65ee82
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-08 20:32:51 +0900

    fix(Study): register event

--- src/main/java/me/saidbysolo/study/Study.java
+++ src/main/java/me/saidbysolo/study/Study.java
@@ -8,7 +8,7 @@ public final class Study extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getLogger().info("Success");
-        getServer().getPluginManager().registerEvent(new FastRespawn(this), this);
+        getServer().getPluginManager().registerEvents(new Listener(), this);
         getCommand("O").setExecutor(new Command(this));
         getCommand("X").setExecutor(new Command(this));
         getCommand("start").setExecutor(new Command(this));
