c733d171428978f4ffcdf165a4c193fc2131ccc3
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-11 17:05:28 +0900

    feat(Study): add config

--- src/main/java/me/saidbysolo/study/Study.java
+++ src/main/java/me/saidbysolo/study/Study.java
@@ -1,11 +1,14 @@
 package me.saidbysolo.study;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Study extends JavaPlugin {
+    FileConfiguration config = getConfig();
 
     @Override
     public void onEnable() {
+        getServer().getLogger().info(config.getString("wtfisthis"));
         getServer().getLogger().info("Success");
         getServer().getPluginManager().registerEvents(new Listener(), this);
         getCommand("O").setExecutor(new Command(this));
