f3b9e4c29461e5745525e9db2d46199709e72555
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-12 16:16:30 +0900

    feat(Listener): add interract event

--- src/main/java/me/saidbysolo/study/Listener.java
+++ src/main/java/me/saidbysolo/study/Listener.java
@@ -1,9 +1,9 @@
 package me.saidbysolo.study;
 
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class Listener implements org.bukkit.event.Listener {
@@ -16,6 +16,12 @@ public class Listener implements org.bukkit.event.Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
         Player player = (Player) event.getPlayer();
-        player.sendMessage(player.getLocation().subtract(0, 1, 0).getBlock().toString());
+        player.sendMessage(player.getLocation().subtract(0, 1, 0).getBlock().getType().toString());
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Player player = (Player) event.getPlayer();
+        player.sendMessage(event.getAction().toString());
     }
 }
