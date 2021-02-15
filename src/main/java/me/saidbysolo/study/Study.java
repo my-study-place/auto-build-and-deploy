070847a21efe767fe7807d71a50ea3ae82b79275
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-15 19:44:36 +0900

    hotfix(study): remove null

--- src/main/java/me/saidbysolo/study/Study.java
+++ src/main/java/me/saidbysolo/study/Study.java
@@ -6,8 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Study extends JavaPlugin {
     FileConfiguration config = getConfig();
-    public Location firstLocation = null;
-    public Location secondLocation = null;
+    public Location firstLocation;
+    public Location secondLocation;
 
     @Override
     public void onEnable() {
